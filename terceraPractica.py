d37f4102c4092b574dc38de5d5ae2438f0b2355a
Yaki55 <mely020202@gs.utm.mx>
2024-10-18 08:50:35 -0600

    imadjust implementado

--- terceraPractica.py
+++ terceraPractica.py
@@ -3,7 +3,19 @@ import cv2
 altoContraste = cv2.imread('primeraImagen.jng')
 bajoContraste = cv2.imread('segundaImagen.png')
 pocaIluminacion = cv2.imread('terceraImagen.png')
+pocaIluminacion2 = cv2.imread('cuartaImagen',0)
 
+def imadjust(F,range_in=(0,1),range_out=(0,1),gamma=1):
+    G = (((F - range_in[0]) / (range_in[1] - range_in[0])) ** gamma) * (range_out[1] - range_out[0]) + range_out[0]
+    return G
+
+
+negativo = imadjust(altoContraste, (0,1), (1,0))
+_, ((ax0, ax1)) = subplots(2, 2, figsize=(10, 10))
+
+ax1.imshow(negativo, cmap="gray")
+ax1.set_title("negativo")
+ax1.set_axis_off()
 
 
 cv2.imshow('img', entrada)
